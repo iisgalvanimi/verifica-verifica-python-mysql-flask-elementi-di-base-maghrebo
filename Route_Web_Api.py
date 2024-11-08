79dd0b6d80fc3332f843839b639a62394b68a31b
Amine Raouf <raouf.amine@iisgalvanimi.edu.it>
2024-11-08 11:10:44 +0000

    commit 3

--- Route_Web_Api.py
+++ Route_Web_Api.py
@@ -42,3 +42,31 @@ def stampaDati():
 @app.route("/")
 def homeRoute():
     return "Buongiorno, questa è la route di default. Per avere la lista dei rettili, inserisci come route /dati"
+
+
+
+#-----------------------------------------------------------------------------
+#Definizione della funzione per il metodo POST
+
+def aggiungiRettile(data):
+    query = "INSERT INTO rettili (nome, famiglia, dimensioni, habitat, alimentazione) VALUES (%s, %s, %s, %s, %s)"
+    valori = (data['nome'], data['famiglia'], data['dimensioni'], data['habitat'], data['alimentazione'])
+    mycursor.execute(query, valori)
+    mydb.commit()
+    return mycursor.rowcount
+
+@app.route("/add", methods=["POST"])
+def aggiunta():
+    data = request.json
+    if not data:
+        return jsonify({'message': 'Nessun dato fornito'}), 400
+
+    required_fields = ['nome', 'famiglia', 'dimensioni', 'habitat', 'alimentazione']
+    if not all(field in data for field in required_fields):
+        return jsonify({'message': 'Probabilmente i dati sono mancanti o errati'}), 400
+
+    rows_inserted = aggiungiRettile(data)
+    if rows_inserted == 1:
+        return jsonify({'message': 'congratulazioni, il rettile e stato inserito con successo'}), 201
+    else:
+        return jsonify({'message': 'Errore durante l inserimento del rettile'}), 500
